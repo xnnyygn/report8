bbb486f9a040a63502e46c2b5197636d52441682
xnnyygn <xnnyygn@gmail.com>
2014-11-05 21:20:35 +0800

    finish deploy with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ namespace :deploy do
 
   desc 'reload the database with seed data'
   task :seed do
+    deploy.migrations
     run 'cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}'
   end
 end
